d2232f2b64c46d9385141eb036fbe8ea18819040
Andreas Berghaus <andreas@bergha.us>
2013-07-02 12:32:13 +0200

    added preldr

---
+++ jquery.preldr.js
@@ -0,0 +1,63 @@
+/**
+ * @name jquery.preloader.js
+ * @author Andreas Berghaus <andreas@bergha.us>
+ *
+ * jquery.preloader.js, utf-8, 2013-04-17
+ */
+
+jQuery( function ( $ ) {
+  "use strict";
+
+	$.fn.preldr = function ( callback ) {
+		var urls = [],
+			$this = $( this );
+
+		this.each( function () {
+			if ( typeof this === 'string' ) {
+				urls.push( this );
+				return true;
+			}
+			$( this ).find( 'img' ).each( function () {
+				urls.push( $( this ).attr( 'src' ) );
+			} );
+		} );
+
+		$.preldr( urls, function () {
+			if ( typeof callback === 'function' ) {
+				callback( $this );
+			}
+		} );
+	};
+
+	$.preldr = function ( urls, callback ) {
+		var $urls = $( urls ),
+			loading = urls.length,
+			loaded;
+
+		loaded = function () {
+			if ( typeof callback === 'function' ) {
+				callback();
+			}
+		};
+
+		$urls.each( function () {
+			var url = this;
+			$( new Image() )
+			.one( 'load error', function () {
+				loading--;
+				if ( loading === 0 ) {
+					loaded();
+				}
+			} )
+			.attr( 'src', url )
+			.each( function () {
+				if ( this.complete ) $( this ).trigger( 'load' );
+			} );
+		} );
+
+		if ( $urls.length === 0 ) {
+			loaded();
+		}
+	};
+
+} );
