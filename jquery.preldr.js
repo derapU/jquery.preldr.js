18b7edb672cefc74c08217d6fd41ca5e9d5ca1d0
Andreas Berghaus <andreas@bergha.us>
2017-08-31 16:56:19 +0200

    + import jquery, webpack style

--- jquery.preldr.js
+++ jquery.preldr.js
@@ -5,55 +5,59 @@
  * jquery.preloader.js, utf-8, 2013-04-17
  */
 
-if ( undefined === jQuery ) { console.error( 'jQuery is required. #preldr' ); return; }
+import jQuery from 'jquery';
 
-( function ( $ ) {
-	"use strict";
-	$.fn.preldr = function ( callback ) {
-		var urls = [],
-			$this = $( this );
+if ( undefined === jQuery ) { console.error( 'jQuery is required. #preldr' ); }
+else {
+	( function ( $ ) {
+		"use strict";
+		$.fn.preldr = function ( callback ) {
+			var urls = [],
+				$this = $( this );
 
-		this.each( function () {
-			if ( typeof this === 'string' ) {
-				urls.push( this );
-				return true;
-			}
-			$( this ).find( 'img' ).each( function () {
-				urls.push( $( this ).attr( 'src' ) );
+			this.each( function () {
+				if ( typeof this === 'string' ) {
+					urls.push( this );
+					return true;
+				}
+				$( this ).find( 'img' ).each( function () {
+					urls.push( $( this ).attr( 'src' ) );
+				} );
 			} );
-		} );
 
-		$.preldr( urls, function () {
-			( callback || function () {} ).apply( undefined, [$this] );
-		} );
-	};
-
-	$.preldr = function ( urls, callback ) {
-		var loading = urls.length,
-			loaded;
-
-		loaded = function () {
-			( callback || function () {} ).apply( undefined, [] );
+			$.preldr( urls, function () {
+				( callback || function () {} ).apply( undefined, [$this] );
+			} );
 		};
 
-		$.each( urls, function () {
-			var url = this;
-			$( '<img />' )
-			.one( 'load', function ( e ) {
-				loading--;
-				if ( loading === 0 ) {
-					loaded( this );
-				}
-			} )
-			.one( 'error', function ( e ) {
-				console.error( 'unable to load image: ' + url );
-				loaded( this );
-			} )
-			.attr( 'src', url );
-		} );
+		$.preldr = function ( urls, callback ) {
+			var loading = urls.length,
+				loaded;
 
-		if ( urls.length === 0 ) {
-			loaded();
-		}
-	};
-} ( jQuery ) );
+			loaded = function () {
+				( callback || function () {} ).apply( undefined, [] );
+			};
+
+			$.each( urls, function () {
+				var url = this;
+				$( '<img />' )
+				.one( 'load', function ( e ) {
+					loading--;
+					if ( loading === 0 ) {
+						loaded( this );
+					}
+				} )
+				.one( 'error', function ( e ) {
+					console.error( 'unable to load image: ' + url );
+					loaded( this );
+				} )
+				.attr( 'src', url );
+			} );
+
+			if ( urls.length === 0 ) {
+				loaded();
+			}
+		};
+	} ( jQuery ) );
+}
+
